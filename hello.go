c8473af686f1c831b8e0051c362971273324f527
HoffsMH <hecker.matthew@gmail.com>
2018-04-17 08:42:32 -0400

    next echo

--- hello.go
+++ hello.go
@@ -6,16 +6,5 @@ import (
 )
 
 func main() {
-	var s, sep string
-  
-  //  i++ is not an expression only a statement
-  // also cannot ++i
-  i := 1
-  // apparently have  to include empty semicolon for init
-  for i < len(os.Args) {
-    s += sep + os.Args[i]
-    sep = " "
-    i++
-  }
-  fmt.Println(s)
+	
 }
